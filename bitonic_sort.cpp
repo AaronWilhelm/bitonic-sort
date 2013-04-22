ef2c299139bd020ee300b13f0d2f89a848079eca
Aaron Wilhelm <ajw7v3@mst.edu>
2013-04-22 18:03:23 -0500

    Add licensing stuff, add an overly simple README

--- bitonic_sort.cpp
+++ bitonic_sort.cpp
@@ -1,4 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////////
+// This file is part of bitonic-sort
+//
+// bitonic-sort is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//  bitonic-sort is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with bitonic-sort.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Copyright 2013 Aaron Wilhelm
+///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
 // @file   bitonic_sort.cpp
 // @author Aaron Wilhelm
 // @brief  Implementation for bitonic sort algorithm using OpenMPI
